61847cf5578ced00d1cab94de172a0f51149f3a5
Nexora Technologies <nexoratechnologies.ke@gmail.com>
2026-04-03 11:40:25 +0000

    Try fixing this error: `Console Error: Invalid DOM property `crossorigin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">{children}</body>
