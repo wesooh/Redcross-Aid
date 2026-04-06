3e48162a155c8dbfbd3e538afdb3edd70fe34dd9
Nexora Technologies <nexoratechnologies.ke@gmail.com>
2026-04-06 06:53:59 +0000

    why am i not seeing the link to reset password here in the login because

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -59,6 +59,12 @@ export default function LoginPage() {
                         <div className="space-y-2">
                             <div className="flex items-center">
                                 <Label htmlFor="password">Password</Label>
+                                <Link
+                                    href="/auth/reset-password"
+                                    className="ml-auto inline-block text-sm underline hover:text-primary"
+                                >
+                                    Forgot your password?
+                                </Link>
                             </div>
                             <div className="relative">
                                 <Input 
